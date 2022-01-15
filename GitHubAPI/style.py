35ba4b861adbc75cbb4e4faa6ef594ebb9fa9d80
Aislinn Addison-Smyth <72949599+aislinnsmyth@users.noreply.github.com>
2022-01-15 12:57:08 +0000

    Add files via upload

--- GitHubAPI/style.py
+++ GitHubAPI/style.py
@@ -1,5 +1,6 @@
 from github import Github
-#plt.style.use('classic')
+import requests
+import json
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -8,10 +9,24 @@ sns.set()
 
 
 token = input("access_token")
+url = "https://api.github.com/user/repos"
+
 g = Github(token)
 
+headers = {
+    "Authorization":"token {}".format(token)
+}
+data = {
+    "visibility" : "all",
+    "type" : "all",
+    "sort" : "full_name",
+    "direction" : "asc"
+}
 
+#Attempt to create a graph nbased off info in each repository
 def GraphCreation(repo):
+    output = requests.get(url,data=json.dumps(data),headers=headers)
+    output
     data = np.random.multivariate_normal([0, 0], [[5, 2], [2, 2]], size=2000)
     data = pd.DataFrame(data, columns=[repo.name,repo.created_at,repo.pushed_at,repo.language,repo.size])
 
@@ -23,16 +38,18 @@ def GraphCreation(repo):
 #"Date created", "Date Last Pushed",, "Language" "Repo Size"])
 #plt.show()
 
-def getCommits(repo):
+
+#Idea of code below referenced from https://github.com/PyGithub/PyGithub
+def commitsToRepo(repo):
     try:
         if repo.get_commits() is not None:
-            commits = repo.get_commits().totalCount
+            totalCommits = repo.get_commits().totalCount
             print("Number of commits:")
-            print(commits)
+            print(totalCommits)
     except:
         print("No commits made to this repository")
 
-#def commits_of_repo_github(repo, owner, api):
+#def commits_of_repo_github(repo, owner, api):          code reference from https://towardsdatascience.com/introduction-to-git-data-extraction-and-analysis-in-python-e7e2bf9b4606
 #    commits = []
 #    next = True
 #    i = 1
@@ -47,13 +64,11 @@ def getCommits(repo):
 #                next = False
 #        i = i + 1
 #    return commits
-
-print("-    -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -")
+print("****************************************************************************")
 for repo in g.get_user().get_repos():
     print("Repository name:",repo.name)
-    print("Date created", repo.created_at, "Last pushed commit",repo.pushed_at)
-    print("Language:",repo.language)
-    print("Size:",repo.size)
-    getCommits(repo)
+    print("Date created", repo.created_at, "    Last pushed commit",repo.pushed_at)
+    print("Language:",repo.language,       "    Size:",repo.size)
+    commitsToRepo(repo)
     #print(graphCreation(repo))
-    print("-    -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -")
+    print("***********************************************************************")
